1200f1c2f8aa863280e48ccf8cfb0e88f1edf7d7
alsatarysamah <alsatary.samah@yahoo.com>
2025-01-29 14:08:32 +0300

    use react hook form

--- app/issues/new-issue/page.tsx
+++ app/issues/new-issue/page.tsx
@@ -1,14 +1,41 @@
 'use client';
-import { Button, Flex, TextArea, TextField } from '@radix-ui/themes';
+import { Button, TextArea, TextField } from '@radix-ui/themes';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
 
+
+interface issue {
+  title: string;
+  description: string;
+}
 const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<issue>()
+  const router = useRouter()
+  const handleFromSubmit = async (data: issue) => {
+    try {
+
+      const newIssue = await axios.post("/api/issues", data)
+      router.push('/issues')
+    } catch (error) {
+      console.log("🚀 ~ handleSubmit ~ error:", error)
+
+    }
+
+  }
   return (
-    <div className='max-w-xl space-y-3'>
-      <TextField.Root radius="large" placeholder="Title" />
-      <TextArea placeholder='Description' />
+    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(handleFromSubmit)}>
+      <TextField.Root radius="large" placeholder="Title" {...register('title')} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => {
+          return <TextArea placeholder='Description'  {...field} />
+        }}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
